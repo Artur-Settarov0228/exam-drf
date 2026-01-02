b1868c6aedd07677763cc2650d8a957876fcb99e
Artur-Settarov0228 <settarovartur2@gmail.com>
2026-01-02 18:05:00 +0500

    user models

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
-# Create your models here.
+class CostumUser(AbstractUser):
+
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", 'admin'
+        DOCTOR = "DOCTOR", 'doctor'
+        PATIENT = "PATIENT", 'patient'
+    
+    role = models.CharField(
+            max_length=15,
+            choices = Role.choices,
+            default = Role.PATIENT
+        )
+    
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
